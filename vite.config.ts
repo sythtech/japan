f8a5d209e4d2fbb429c71c98af5fa039c868d42e
syth <annanaydoesgaming@gmail.com>
2026-08-21 14:04:11 +0800

    made a mistake

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: set `base` to "/<your-repo-name>/" for GitHub Pages
-// (unless the repo is named "<username>.github.io", in which case use "/")
+
 export default defineConfig({
   plugins: [react()],
-  base: "/japan-portfolio/",
+  base: "/japan/",
 });
